d5b8d8bb91c8577f53598110f4654a35b42fb940
Issac Arzate Soto <issac@MacBook-Pro-de-Issac.local>
2018-06-12 01:23:52 -0500

    Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,65 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import {IntroPage} from "../pages/intro/intro";
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyA2QCVwtr298TPcop6wK0gBShMylWnvWIc",
+  authDomain: "sendi-9f11f.firebaseapp.com",
+  databaseURL: "https://sendi-9f11f.firebaseio.com",
+  projectId: "sendi-9f11f",
+  storageBucket: "sendi-9f11f.appspot.com",
+  messagingSenderId: "238310976567"
+};
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {LoginPage} from "../pages/login/login";
+import { UsuarioProvider } from '../providers/usuario/usuario';
+
+import { Facebook } from '@ionic-native/facebook';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    LoginPage,
+    IntroPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    LoginPage,
+    IntroPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    AngularFireDatabase,
+    UsuarioProvider,
+    Facebook,
+    IonicStorageModule,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Credenciales, UsuarioProvider} from "../../providers/usuario/usuario";
+
+import { AngularFireAuth } from 'angularfire2/auth';
+import {LoginPage} from "../login/login";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  user: Credenciales = {};
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public usuarioProv: UsuarioProvider, private AFauth: AngularFireAuth) {
 
+    console.log(this.usuarioProv.usuario);
+    this.user = this.usuarioProv.usuario;
+
+    //Chaecar el estado de autenticacion del usaurio
+    this.AFauth.authState.subscribe( user => {
+      console.log("AFAuth!!!");
+      console.log(JSON.stringify(user));
+    });
+
+  }
+
+  salir(){
+    this.AFauth.auth.signOut().then( res => {
+      this.usuarioProv.usuario = {};
+      this.navCtrl.setRoot(LoginPage);
+    });
   }
 
 }
